87705d1d4e528390e9541559b441f067dc91ec3c
Autoscuola ABA <80291064+autoscuolaaba@users.noreply.github.com>
2025-12-29 14:55:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/prenota-esame-teoria/',  // 👈 AGGIUNTO
       server: {
         port: 3000,
         host: '0.0.0.0',
